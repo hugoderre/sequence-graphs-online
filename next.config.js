3aab9b4addff654e9b850b7af3dd641c9f6ae47c
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-08-05 05:42:30 +0200

    feat: add webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
 }
 
 module.exports = nextConfig
+
+const withBundleAnalyzer = require( '@next/bundle-analyzer' )( {
+	enabled: process.env.ANALYZE === 'true',
+} )
+module.exports = withBundleAnalyzer( {} )
